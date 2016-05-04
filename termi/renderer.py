1f288d1deb8c7125358b11c682cd0b3dbd1ce438
rr- <rr-@sakuya.pl>
2016-05-04 21:06:02 +0200

    renderer: remove custom quantization cruft
    For some reason the results look much better now...?

--- termi/renderer.py
+++ termi/renderer.py
@@ -4,55 +4,6 @@ import sys
 from PIL import Image
 from termi import term
 
-def _quantize_native(source_image, palette):
-    return source_image.quantize(palette=palette, method=1)
-
-def _quantize_floyd_steinberg(source_image, palette):
-    def _from_rgb(color):
-        return numpy.array([color[0], color[1], color[2]], numpy.float)
-
-    # Floyd-Steinberg
-    def _dither(pix, size, x, y, quant_error):
-        width, height = size
-        if x + 1 < width:
-            pix[x+1, y] += quant_error * 7/16
-        if y + 1 < height:
-            if x > 0:
-                pix[x-1, y+1] += quant_error * 3/16
-            pix[x, y+1] += quant_error * 5/16
-            if x + 1 < width:
-                pix[x+1, y+1] += quant_error / 16
-
-    target_image = Image.new(mode='P', size=source_image.size)
-    source_pixels = source_image.load()
-    target_pixels = target_image.load()
-
-    source_pixels_copy = {}
-    for x in range(source_image.width):
-        for y in range(source_image.height):
-            source_pixels_copy[x, y] = _from_rgb(source_pixels[x, y])
-
-    palette_colors = numpy.array(list(zip(*(iter(palette.getpalette()),) * 3)))
-    for x in range(source_image.width):
-        for y in range(source_image.height):
-            source_pixel = source_pixels_copy[x, y]
-            index = numpy.linalg.norm(palette_colors - source_pixel, axis=1).argmin()
-            target_pixels[x, y] = (index,)
-            target_pixel = palette_colors[index]
-            _dither(
-                source_pixels_copy,
-                source_image.size,
-                x, y,
-                source_pixel - target_pixel)
-    return target_image
-
-
-try:
-    import numpy
-    _quantize = _quantize_floyd_steinberg
-except ImportError:
-    _quantize = _quantize_native
-
 def _fit_rectangle(source_rectangle, target_rectangle):
     source_ar = source_rectangle[0] / source_rectangle[1]
     target_ar = target_rectangle[0] / target_rectangle[1]
@@ -84,16 +35,10 @@ def resize_image(image, container_size, glyph_ar):
     image = image.convert('RGB')
     return image.resize(final_size, resample=Image.LANCZOS)
 
-def render_image(
-        image,
-        size,
-        glyph_ar,
-        palette_image,
-        output_strategy,
-        quantize_strategy=_quantize_native):
+def render_image(image, size, glyph_ar, palette_image, output_strategy):
     image = resize_image(image, size, glyph_ar)
     if palette_image:
-        image = quantize_strategy(image, palette=palette_image)
+        image = image.quantize(palette=palette_image)
     pixels = image.load()
     output = ''
     for y in range(0, image.height, 2):
